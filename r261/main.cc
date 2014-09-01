bdbd3c5f6b8a1de8ae1c3f7ee91dff7ba2eae683
Joonas Pihlajamaa <joonas.pihlajamaa@iki.fi>
2014-09-01 16:40:41 +0300

    Round 261

--- r261/main.cc
+++ r261/main.cc
@@ -11,40 +11,43 @@ using namespace std;
 
 typedef long long LL;
 
-bool inc(int *b, int k, int d) {
-	if(!d) return false;
-	if(*b == k) {
-		*b = 1;
-		return inc(b+1, k, d-1);
-	} else b[0]++;
-	return true;
-}
+// store lengths for decreasing weights retaining previous value
+struct Vertex {
+	int len = 0, prevLen = 0, weight = 0;
+
+	void setLen(int w, int l) {
+		if(w < weight) prevLen = len; // store old value
+		len = l, weight = w;
+	}
+
+	int getLen(int w) {
+		return (w <= weight) ? len : prevLen; // new w starts at len
+	}
+};
 
 int main() {
-	int n, k, d, bus[1000];
-	vector< vector<int> > plan;
+	int n, m, u, v, w, len, bestLen = 0;
 
-	cin >> n >> k >> d;
+	cin >> n >> m;
 
-	plan.resize(n);
+    vector< tuple<int,int,int> > edges;
+	vector<Vertex> ve(n+1);
 
-	FOR(i,0,d) bus[i] = 1;
-
-	FOR(j,0,n) {
-		plan[j].insert(plan[j].begin(), bus, bus+d);
-		//FOR(i,0,d) plan[j].push_back(bus[i]);
-		//FOR(i,0,d) cout << bus[i] << " ";
-		//cout << endl;
-		if(!inc(bus, k, d) && j < n-1) {
-			cout << -1;
-			return 0;
-		}
+	REP(m) {
+		cin >> u >> v >> w;
+		edges.push_back(make_tuple(w, u, v));
 	}
 
-	FOR(i,0,d) {
-		FOR(j,0,n) cout << plan[j][i] << " ";
-		cout << endl;
+	sort(edges.rbegin(), edges.rend());
+
+	for(auto t : edges) {
+		w = get<0>(t), u = get<1>(t), v = get<2>(t);
+		len = max(ve[u].getLen(w), ve[v].getLen(w+1) + 1);
+		ve[u].setLen(w, len);
+		bestLen = max(bestLen, len);
 	}
+
+	cout << bestLen << endl;
 
 	return 0;
 }

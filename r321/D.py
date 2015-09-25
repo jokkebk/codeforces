0327e78356c0bb3becaf8c9edff728241e417f98
Joonas Pihlajamaa <joonas.pihlajamaa@iki.fi>
2015-09-25 13:48:44 +0300

    321D Python optimization, still too slow

--- r321/D.py
+++ r321/D.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 n, m, k = (int(v) for v in input().split())
 
 masks = [list() for i in range(n+1)]
@@ -7,7 +9,7 @@ dp = [[0]*(1<<n) for i in range(n)]
 
 for i,a in enumerate(input().split()): dp[i][0] = int(a)
 
-sat = {}
+sat = {p: 0 for p in product(range(n), repeat=2)}
 for i in range(k):
     x, y, c = (int(v) for v in input().split())
     sat[(x-1,y-1)] = c
@@ -16,9 +18,8 @@ for bits in range(1,m+1):
     for ma in masks[bits]:
         for j in range(n):
             if ma & (1<<j): continue # not a new bit
-            dp[j][ma] = max(dp[b][ma-(1<<b)] +
-                        dp[j][0] + sat.setdefault((b,j), 0) \
-                                for b in range(n) if (ma&(1<<b)))
+            dp[j][ma] = dp[j][0] + max(dp[b][ma-(1<<b)] +
+                        sat[(b,j)] for b in range(n) if (ma&(1<<b)))
 
 best = 0
 for ma in masks[m]:

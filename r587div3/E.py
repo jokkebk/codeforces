7a705bd81b851e186edfc73aaa10831bc789b0ff
Joonas Pihlajamaa <joonas.pihlajamaa@iki.fi>
2019-09-23 00:07:57 +0300

    E cleaned

--- r587div3/E.py
+++ r587div3/E.py
@@ -19,22 +19,6 @@ def num(n): # return s[n-1] where s = '1234567891011..n'
         if n <= seglen: return str(10**(i-1) + (n-1)//i)[(n-1)%i]
         else: n -= seglen
 
-#def gen(N):
-#    a = []
-#    for i in range(1,N): a += [str(j) for j in range(1,i+1)]
-#    return ''.join(a)
-#
-#if __name__ == "__main__":
-    #s = gen(4000)
-    #err = []
-    #for i in range(1000):
-    #    k = randint(1, len(s))
-    #    sk = num(k-l(bs(k)))
-    #    if s[k-1] != str(sk):
-    #        err.append(k)
-    #        print(k, s[k-1], 'vs', sk)
-    #exit(1)
-
 q = int(input())
 for _ in range(q):
     k = int(input())

37719bb7ce4bcacfc4988ff316a349d8bd8c5f78
Joonas Pihlajamaa <joonas.pihlajamaa@iki.fi>
2019-11-03 17:58:47 +0200

    C improved

--- r597/C.py
+++ r597/C.py
@@ -1,24 +1,18 @@
-import operator as op
 import re
 from functools import reduce, lru_cache
 
-def ncr(n, r):
-    r = min(r, n-r)
-    numer = reduce(op.mul, range(n, n-r, -1), 1)
-    denom = reduce(op.mul, range(1, r+1), 1)
-    return numer // denom
+M = int(1e9+7)
 
 @lru_cache(maxsize=None)
 def w(n):
-    if n < 5: return sum(ncr(n-i, i) for i in range(n//2+1))
+    if n < 5: return (1, 1, 2, 3, 5, 8)[n]
     a = n//2
     b = n-a
-    return (w(a)*w(b) + w(a-1)*w(b-1)) % 1000000007
+    return (w(a)*w(b) + w(a-1)*w(b-1)) % M
 
 s = input().strip()
 
 if 'm' in s or 'w' in s: print(0)
 else:
     s = [w(len(l)) for l in re.findall(r'u+|n+', s)]
-    print(reduce(op.mul, s, 1) % 1000000007)
-
+    print(reduce(lambda a,b: a * b % M, s, 1))
